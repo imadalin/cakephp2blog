236ed91d811ef07df459b7a25239dcd239ea4de3
Madalin Ignisca <madalin.ignisca@gmail.com>
2014-04-21 14:25:32 +0300

    finished first step, added Codekit and phpunit for next lessons.

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -105,3 +105,8 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
+
+/**
+ * Adding awesome
+ */
+CakePlugin::load('DebugKit');
